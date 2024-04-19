2bf3ff1586075a0bddd740e9fbd75e5d23a86809
mintaekim <kmintae33@gmail.com>
2024-04-19 18:25:27 +0800

    set up GUI using JFrame

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class Calculator {
+
+    public Calculator() {
+        JFrame frame = new JFrame(); // create frame for the container
+
+        JPanel panel = new JPanel(); // create panel for the layout
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our Calculator");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello calculator");
+        System.out.println("Start calculator");
+        new Calculator();
     }
 }
