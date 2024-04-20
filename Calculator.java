91c8ab379d6f2adca09e9a0087be2b5e600cf26b
mintaekim <kmintae33@gmail.com>
2024-04-20 17:18:42 +0800

    Declare variables

--- Calculator.java
+++ Calculator.java
@@ -9,31 +9,50 @@ public class Calculator implements ActionListener {
     private JLabel label;
     private JFrame frame;
     private JPanel panel;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functiButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
 
     public Calculator() {
-        frame = new JFrame(); // create frame for the container
+        frame = new JFrame("Calculator"); // create frame for the container
 
-        JButton button = new JButton("Click me");
-        button.addActionListener(this);
-        label = new JLabel("Number of clicks: 0");
+        // JButton button = new JButton("Click me");
+        // button.addActionListener(this);
+        // label = new JLabel("Number of clicks: 0");
 
-        panel = new JPanel(); // create panel for the layout
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
-        panel.add(label);
+        // panel = new JPanel(); // create panel for the layout
+        // panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        // panel.setLayout(new GridLayout(0, 1));
+        // panel.add(button);
+        // panel.add(label);
+        // frame.add(panel, BorderLayout.CENTER);
 
-        frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Our Calculator");
-        frame.pack();
+        // frame.pack();
+
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+
+        frame.add(textField);
+
         frame.setVisible(true);
 
     }
 
     public static void main(String[] args) {
         System.out.println("Start calculator");
-        new Calculator();
+        Calculator calc = new Calculator();
     }
 
     @Override
