d3e2aea79c07853687bd3d689670e4ef663b08cd
mintaekim <kmintae33@gmail.com>
2024-04-25 23:25:11 +0800

    clean up

--- Calculator.java
+++ Calculator.java
@@ -5,8 +5,6 @@ import java.awt.event.ActionListener;
 
 public class Calculator implements ActionListener {
 
-    private int count = 0;
-    private JLabel label;
     private JFrame frame;
     private JPanel panel;
     JTextField textField;
@@ -23,19 +21,7 @@ public class Calculator implements ActionListener {
     public Calculator() {
         frame = new JFrame("Calculator"); // create frame for the container
 
-        // JButton button = new JButton("Click me");
-        // button.addActionListener(this);
-        // label = new JLabel("Number of clicks: 0");
-
-        // panel = new JPanel(); // create panel for the layout
-        // panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        // panel.setLayout(new GridLayout(0, 1));
-        // panel.add(button);
-        // panel.add(label);
-        // frame.add(panel, BorderLayout.CENTER);
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // frame.pack();
 
         frame.setSize(420, 550);
         frame.setLayout(null);
@@ -117,14 +103,12 @@ public class Calculator implements ActionListener {
     }
 
     public static void main(String[] args) {
-        System.out.println("Start calculator");
+        System.out.println("Starting calculator...");
         Calculator calc = new Calculator();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // count++;
-        // label.setText("Number of clicks: " + count);
         for (int i = 0; i < 10; i++) {
             if (e.getSource() == numberButtons[i]) {
                 textField.setText(textField.getText().concat(String.valueOf(i)));
